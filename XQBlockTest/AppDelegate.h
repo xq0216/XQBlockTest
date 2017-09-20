5d69ee7f9fdec869ad177f101bc01a5afb9365c0
laixuefei <laixuefei@g7.com.cn>
2017-09-20 14:47:53 +0800

    Initial Commit

---
+++ XQBlockTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XQBlockTest
+//
+//  Created by LaiXuefei on 2017/9/20.
+//  Copyright © 2017年 lxf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XQBlockTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XQBlockTest
+//
+//  Created by LaiXuefei on 2017/9/20.
+//  Copyright © 2017年 lxf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
